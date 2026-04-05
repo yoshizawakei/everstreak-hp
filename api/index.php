019cbdd15d53b7d53e51def72994f39bb2914c89
yoshizawa kei <kei.yszwa.2525@gmail.com>
2026-04-05 21:00:05 +0900

    20260405-5

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+// Vercelは /tmp 以外書き込めないので、キャッシュ先などを強制的に変更する
+mkdir('/tmp/storage/framework/views', 0755, true);
+mkdir('/tmp/storage/framework/cache', 0755, true);
+mkdir('/tmp/storage/framework/sessions', 0755, true);
+
 require __DIR__ . '/../public/index.php';
