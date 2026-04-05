bb3385e47edd84d8dab3ce62499e8c2a9fa6dd9d
yoshizawa kei <kei.yszwa.2525@gmail.com>
2026-04-05 21:04:37 +0900

    add api/index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,13 @@
 <?php
-// Vercelは /tmp 以外書き込めないので、キャッシュ先などを強制的に変更する
-mkdir('/tmp/storage/framework/views', 0755, true);
-mkdir('/tmp/storage/framework/cache', 0755, true);
-mkdir('/tmp/storage/framework/sessions', 0755, true);
+// すでに存在するか確認してから作成するように修正
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/cache')) {
+    mkdir('/tmp/storage/framework/cache', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/sessions')) {
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+}
 
 require __DIR__ . '/../public/index.php';
